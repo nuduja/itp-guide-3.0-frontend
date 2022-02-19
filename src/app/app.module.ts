4f76e733aa066795d6cba8b76feaf3e6ce7af28a
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-02-19 16:10:58 +0530

    Add modules to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataService } from './core/data.service';
 import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
@@ -10,9 +14,12 @@ import { TodoComponent } from './todo/todo.component';
     TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
